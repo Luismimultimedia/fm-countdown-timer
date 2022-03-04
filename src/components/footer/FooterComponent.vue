a690f10c2bbb44dcd4d72ff4a6120df9a66a5d74
Luis González <lgonzalez@serempre.com>
2022-03-04 09:17:44 -0500

    Initial commit

---
+++ src/components/footer/FooterComponent.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="footer__container">
+      <figure class="footer__images">
+          <img 
+              src="../../assets/images/icon-facebook.svg"
+              alt="facebook social media icon"
+          />
+          <img 
+              src="../../assets/images/icon-pinterest.svg"
+              alt="pinterest social media icon"
+          />
+          <img 
+              src="../../assets/images/icon-instagram.svg"
+              alt="instagram social media icon"
+          />
+
+      </figure>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'FooterComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+.footer {
+    &__container {
+        align-items: center;
+        background-image: url("/assets/images/pattern-hills.svg");
+        background-repeat: no-repeat;
+        background-size: cover;
+        bottom: 0;
+        display: flex;
+        height: 10rem;
+        justify-content: center;
+        position: absolute;
+        width: 100%;
+    }
+    &__images {
+        display: flex;
+        justify-content: space-between;
+        width: 30%;
+    }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main class="home-page__container">
+    <h1 class="home-page__title">WE'RE LAUNCHING SOON</h1>
+    <footer-component />
+  </main>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import FooterComponent from "@/components/footer/FooterComponent.vue";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  components: { FooterComponent },
+  setup() {
+    const time = new Date();
+
+    return {
+      time,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.home-page {
+  &__container {
+    padding-top: 3rem;
+  }
+  &__title {
+    font-size: 1.5rem;
+    color: var(--color-white);
+    letter-spacing: 0.4rem;
+    text-align: center;
+    margin: 0;
   }
 }
-</script>
+</style>
